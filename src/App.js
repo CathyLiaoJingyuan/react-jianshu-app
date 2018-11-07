5fef41fd36ac9ae529eb3ca1fb355bc8e8718edf
Cathy Liao <cathyliao87@gmail.com>
2018-11-06 20:44:41 -0500

    init proj

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import GlobalStyle from "./style";
+
+class App extends Component {
+  render() {
+    return (
+
+       <div>hi
+           <GlobalStyle/> 
+       </div>
+    
+    );
+  }
+}
+
+export default App;
